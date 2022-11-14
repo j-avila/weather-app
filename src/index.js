47d002350ca7a2b8714739496030c594330f8d6f
j-avila <jose.leon.avila@gmail.com>
2022-11-14 18:33:20 -0300

    updated to react 18 and icons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { StrictMode } from "react"
+import { createRoot } from "react-dom"
+import "./index.scss"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root")
+const root = createRoot(rootElement)
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </StrictMode>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
